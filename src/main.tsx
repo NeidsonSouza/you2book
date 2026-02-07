6a1f242ed77eefa237eb94d5efac7bea9a85cb7c
Neidson Souza <neidson.ds.souza@gmail.com>
2026-02-07 08:44:28 -0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import ChannelDetail from "./ChannelDetail.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Authenticator>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/channel/:channelUrl" element={<ChannelDetail />} />
-        </Routes>
-      </BrowserRouter>
-    </Authenticator>
-  </React.StrictMode>
+  <div>hello neidson nei</div>
 );
